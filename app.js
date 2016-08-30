ba89a67e7588b391ae75ad0191ccd6e1da9e6da4
Bex B <bexb1@hotmail.com>
2016-08-30 11:03:03 +0100

    Heroku push.

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 var express        = require('express');
 var app            = express();
 var environment    = app.get('env');
-const PORT         = process.env.PORT || 3000;
+const PORT          = process.env.PORT || 3000;
 var router         = require('./config/routes');
 var pug            = require('pug');
 
 
 app.use('/', router);
+app.set('view engine', 'pug');
 
 app.set('views', __dirname +'/views');
-app.set('view engine', 'pug');
 
 app.use(express.static(__dirname + '/public'));
 
 
 // Listen for incoming HTTP requests
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Express is listening on port " + PORT + ".");
 });
 
 module.exports = app;
