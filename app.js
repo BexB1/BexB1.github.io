5e972311678058a5d7dde7a6c4d5d743c35e3c12
Bex B <bexb1@hotmail.com>
2016-09-27 09:32:13 +0100

    app.js added

---
+++ app.js
@@ -0,0 +1,23 @@
+var express        = require('express');
+var app            = express();
+var environment    = app.get('env');
+var PORT           = process.env.PORT || 3000;
+var router         = require('./config/routes');
+var pug            = require('pug');
+
+
+app.use('/', router);
+app.set('view engine', 'pug');
+
+app.set('port', (process.env.PORT || 3000));
+app.set('views', __dirname +'/views');
+
+app.use(express.static('public'));
+
+
+// Listen for incoming HTTP requests
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Express is listening on port " + PORT + ".");
+});
+
+module.exports = app;
