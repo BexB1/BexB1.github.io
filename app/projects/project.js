0c193f85fc8530f756a1556ccfcd142897c0f26b
Bex B <bexb1@hotmail.com>
2016-09-29 13:07:54 +0100

    Flex layout added for projects page

--- app/projects/project.js
+++ app/projects/project.js
@@ -5,21 +5,24 @@ exports.Projects = [
         pic: '../pics/projects/Londate.png',
         name: 'Londate',
         blurb: 'A local events-finding app using the Google Maps and Meetup.com APIs.',
-        tech: 'HTML5, CSS3, JavaScript, AngularJS, node.js, ExpressJS, MongoDB'
+        tech: 'HTML5, CSS3, JavaScript, AngularJS, node.js, ExpressJS, MongoDB',
+        url: 'https://github.com/BexB1/WDI_Final_Project'
     },
     {
         id: 2,
         pic: '../pics/projects/FF.png',
         name: 'Final Fantasy: Primal Battle',
         blurb: 'A turn-based RPG modelled on the retro Final Fantasy games.',
-        tech: 'HTML5, CSS3 and JavaScript'
+        tech: 'HTML5, CSS3 and JavaScript',
+        url: 'https://github.com/BexB1/Final-Fantasy-Primal-Battle'
     },
     {
         id: 3,
         pic: '../pics/projects/ThirtyWins.png',
         name: 'ThirtyWins',
         blurb: 'ThirtyWins is a goal-setting app which helps users to build good habits on a daily basis through the setting of personal goals.',
-        tech: 'HTML5, CSS3, JavaScript, jQuery, Ruby, Ruby on Rails'
+        tech: 'HTML5, CSS3, JavaScript, jQuery, Ruby, Ruby on Rails',
+        url: 'https://github.com/BexB1/WDI_21_PROJECT_2'
     }
 ];
 //# sourceMappingURL=project.js.map
