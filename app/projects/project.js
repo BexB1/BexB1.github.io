3299ed50a146d0db3bc749666c32f30483b6a527
Bex B <bexb1@hotmail.com>
2016-10-01 17:29:41 +0100

    Adding another project

--- app/projects/project.js
+++ app/projects/project.js
@@ -23,6 +23,14 @@ exports.Projects = [
         blurb: 'ThirtyWins is a goal-setting app which helps users to build good habits on a daily basis through the setting of personal goals.',
         tech: 'HTML5, CSS3, JavaScript, jQuery, Ruby, Ruby on Rails',
         url: 'https://github.com/BexB1/WDI_21_PROJECT_2'
+    },
+    {
+        id: 4,
+        pic: '../pics/projects/zoogle.jpg',
+        name: 'Zoogle',
+        blurb: "Zoogle is a browser-based game utilising Google's JS Maps API. Zoogle allows the user to collect resources and encounter zombies while walking around central London.",
+        tech: 'HTML, CSS, JavaScript, jQuery, Express, Mongoose, MongoDB and Google Maps API',
+        url: 'https://github.com/tankjem/WDI_21_PROJECT_DOGEMON'
     }
 ];
 //# sourceMappingURL=project.js.map
